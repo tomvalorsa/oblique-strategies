1438fa5ef024f01af5d36c7f503ae1b7e575701f
Tom Valorsa <tomvalorsa@gmail.com>
2018-05-26 09:05:09 +0100

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './index.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        sup
+      </div>
+    );
+  }
+}
+
+export default App;
