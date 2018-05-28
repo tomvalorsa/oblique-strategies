44dd3e64ec2b7e7853c3e7c899b84ba43ec03366
Tom Valorsa <tomvalorsa@gmail.com>
2018-05-28 16:02:07 +0100

    Add real data, some tidying

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Oblique Strategies
 
 TODO
+
+
+card data:
+
+https://traviscj.com/blog/oblique_programming_strategies.html
+https://github.com/RobBlackwell/oblique-strategies-for-programmers
+Original oblique strategies
+me
+
+pretty card back:
+// http://lea.verou.me/css3patterns/#weave
+  // by Jeroen Franse
+- it's a gradient!
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,43 +4,15 @@ import styles from './index.module.scss';
 import Deck from '../Deck';
 import { Strategies } from '../../const';
 import { Strategy } from '../../types';
+import shuffle from 'lodash.shuffle';
 
 /*
   TODO:
-  - source some data
-    - look at creating a programming version in the future?
-  - shuffle initially
-    - use the fisher yates shuffle, write it out myself, tests
-  - pop off the array one by one (i.e. so we don't get duplicates when drawing)
-    - write test so that once it's out there, it's no longer in array
-  - when empty, reshuffle option
-  - add in animations after
-  - how do cards work?
-    - will need stack of cards, then an active/selected card
-    - render them all at once
-    - then when card is selected/drawn, render an active card on top of stack
-    - this will start off like normal item in the stack, but will be animated out
-
-  stack of cards (i.e. render something for each)
-    - not specifically tied to each card
-    - just one thing per card
-
-  card is drawn
-    - top card becomes 'focus' or 'selected'
-    - this is taken off the pile
-    - show in center of screen
-
-  card is dismissed
-    - back to stack of cards, now with one less
-
-  state:
-  - strategies
-    - initial state: a shuffled array
-    - updates: one less record each time, 'pop' one off (without mutating)
-  - selected
-    - initial state: null
-    - updates: receives the 'popped' strategy
-  
+  - css border bug for card
+  - clean everything up
+  - write readme with license (unlicense?)
+  - favicon
+  - cross browser compat
 */
 
 type State = {
@@ -50,13 +22,20 @@ type State = {
 
 class App extends React.Component<{}, State> {
   state = {
-    cards: Strategies,
+    cards: shuffle(Strategies),
     selected: null
   };
 
   drawCard = () => {
     const [selected, ...remaining] = this.state.cards;
     this.setState({ cards: remaining, selected });
+  };
+
+  restart = () => {
+    this.setState({
+      cards: shuffle(Strategies),
+      selected: null
+    });
   };
 
   render() {
@@ -66,9 +45,15 @@ class App extends React.Component<{}, State> {
       <div className={styles.app}>
         <Deck cards={cards} selected={selected} />
 
-        <button className={styles.drawCard} onClick={this.drawCard}>
-          DRAW A CARD
-        </button>
+        {cards.length > 0 ? (
+          <button className={styles.button} onClick={this.drawCard}>
+            DRAW A CARD
+          </button>
+        ) : (
+          <button className={styles.button} onClick={this.restart}>
+            RESTART
+          </button>
+        )}
       </div>
     );
   }
